f8cebaddd1253e84e673a3f20647fd4640f6adf8
adammendak <adam.mendak@gmail.com>
2017-10-25 15:10:17 +0200

    added new entities and JPA relations

--- src/main/java/com/adammendak/recipe/model/Recipe.java
+++ src/main/java/com/adammendak/recipe/model/Recipe.java
@@ -1,7 +1,14 @@
 package com.adammendak.recipe.model;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Recipe {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String description;
     private Integer prepTime;
     private Integer cookTime;
@@ -9,14 +16,28 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-
     //toDo add
-//    private Difficulty difficulty;
+    //    private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
+    @Lob
     private Byte[] image;
+
+    @OneToOne( cascade = CascadeType.ALL)
     private Notes notes;
 
     public String getDescription() {
         return description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setDescription(String description) {
