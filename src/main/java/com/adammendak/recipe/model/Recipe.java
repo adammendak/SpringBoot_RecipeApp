d4eecb4803219884a2b597b0b757a4fcdc2eb141
adammendak <adam.mendak@gmail.com>
2017-10-25 17:58:04 +0200

    final relations manytomany etc.

--- src/main/java/com/adammendak/recipe/model/Recipe.java
+++ src/main/java/com/adammendak/recipe/model/Recipe.java
@@ -16,6 +16,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -26,6 +28,12 @@ public class Recipe {
 
     @OneToOne( cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"),
+    inverseJoinColumns = @JoinColumn( name = "category_id"))
+    private Set<Category> categories;
 
     public String getDescription() {
         return description;
@@ -121,5 +129,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
