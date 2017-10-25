892b1f92c45fc496d1de2b1448a2481716f0cdfd
adammendak <adam.mendak@gmail.com>
2017-10-25 20:39:06 +0200

    testing of repositories in index controller

--- src/main/java/com/adammendak/recipe/controller/IndexController.java
+++ src/main/java/com/adammendak/recipe/controller/IndexController.java
@@ -1,13 +1,37 @@
 package com.adammendak.recipe.controller;
 
+import com.adammendak.recipe.model.Category;
+import com.adammendak.recipe.model.UnitOfMeasure;
+import com.adammendak.recipe.repository.CategoryRepository;
+import com.adammendak.recipe.repository.RecipeRepository;
+import com.adammendak.recipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private RecipeRepository recipeRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeRepository recipeRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.recipeRepository = recipeRepository;
+    }
+
     @RequestMapping({"/", "", "/index"})
     public String getIndexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Fast Food");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByUom("gram");
+
+        System.out.println("Cat id is :" + categoryOptional.get().getId());
+        System.out.println("Uom id is :" + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
--- src/main/java/com/adammendak/recipe/repository/RecipeRepository.java
+++ src/main/java/com/adammendak/recipe/repository/RecipeRepository.java
@@ -3,5 +3,9 @@ package com.adammendak.recipe.repository;
 import com.adammendak.recipe.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+
+    Optional<Recipe> findByDescription (String description);
 }
