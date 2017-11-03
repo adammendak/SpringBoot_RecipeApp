0820f85e9b1949f0ecf4fecff492da59895f8a25
adammendak <adam.mendak@gmail.com>
2017-11-03 15:15:57 +0100

    last tests and added webjars

---
+++ src/test/java/com/adammendak/recipe/repository/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,34 @@
+package com.adammendak.recipe.repository;
+
+import com.adammendak.recipe.model.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByUom() throws Exception {
+
+        UnitOfMeasure uom = unitOfMeasureRepository.findByUom("kubek");
+
+        assertEquals("kubek", uom.getUom());
+    }
+
+}
