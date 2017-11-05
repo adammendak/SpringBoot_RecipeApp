07dcab936584d8646f259b3f891cd233008da029
adammendak <adam.mendak@gmail.com>
2017-11-05 21:42:39 +0100

    fixed save method for recipe

--- src/main/java/com/adammendak/recipe/repository/RecipeRepository.java
+++ src/main/java/com/adammendak/recipe/repository/RecipeRepository.java
@@ -1,10 +1,9 @@
 package com.adammendak.recipe.repository;
-
 import com.adammendak.recipe.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
-
 import java.util.Optional;
 import java.util.Set;
+
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
@@ -13,8 +12,5 @@ public interface RecipeRepository extends CrudRepository<Recipe, Long> {
     Set<Recipe> findAll();
 
     Optional<Recipe> findById(Long l);
-
-    Recipe saveRecipe(Recipe recipe);
-
 
 }
