ccf0fa68ef52e7ee9e8067d64f54ee95dfb5e49c
adammendak <adam.mendak@gmail.com>
2017-11-08 20:54:26 +0100

    added rest of endpoints in ingredientsController

--- src/main/java/com/adammendak/recipe/controller/IngredientController.java
+++ src/main/java/com/adammendak/recipe/controller/IngredientController.java
@@ -1,5 +1,7 @@
 package com.adammendak.recipe.controller;
 
+import com.adammendak.recipe.model.Ingredient;
+import com.adammendak.recipe.model.Recipe;
 import com.adammendak.recipe.service.IngredientService;
 import com.adammendak.recipe.service.RecipeService;
 import com.adammendak.recipe.service.UnitOfMeasureService;
@@ -7,10 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IngredientController {
@@ -27,13 +29,59 @@ public class IngredientController {
         this.unitOfMeasureService = unitOfMeasureService;
     }
 
-    @RequestMapping(value = "/recipe/{id}/ingredients", method = GET)
+    @RequestMapping(value = "/recipe/{id}/ingredients", method = RequestMethod.GET)
     public String getIngredientsList(@PathVariable String id, Model model) {
         logger.info("getting lists of ingredients for recipe no: " + id);
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/ingredient/list";
     }
+
+    @RequestMapping(value = "recipe/{recipeId}/ingredient/show/{id}", method = RequestMethod.GET)
+    public String showRecipeIngredient (@PathVariable String recipeId, @PathVariable String id, Model model) {
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+
+        return "recipe/ingredient/show";
+    }
+
+    @RequestMapping(value = "recipe/{recipeId}/ingredient/new", method = RequestMethod.GET)
+    public String newIngredient(@PathVariable String recipeId, Model model) {
+
+        model.addAttribute("recipe", recipeService.findById(new Long(recipeId));
+        model.addAttribute("uom", unitOfMeasureService.getAll());
+
+        return "recipe/ingredient/ingredientForm";
+    }
+
+    @RequestMapping(value = "recipe/{recipeId}/ingredient/{id}/update", method = RequestMethod.GET)
+    public String updateIngredient(@PathVariable String recipeId, @PathVariable String id, Model model) {
+
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+        model.addAttribute("recipe", recipeService.findById(new Long(recipeId)));
+        model.addAttribute("uom", unitOfMeasureService.getAll());
+
+        return "recipe/ingredient/ingredientForm";
+    }
+
+    @RequestMapping(value = "recipe/{recipeId}/ingredient", method = RequestMethod.POST)
+    public String saveOrUpdateIngredient(@ModelAttribute Ingredient ingredient, @PathVariable String recipeId) {
+
+        Recipe recipe = recipeService.findById(new Long(recipeId));
+
+        Ingredient ingredientToSave = ingredientService.saveIngredient(ingredient, recipe));
+
+        return "redirect:/recipe/" + recipeId + "/ingredient/" + ingredient.getId() + "/show";
+    }
+
+    @RequestMapping(value = "recipe/{recipeId}/ingredient/{id}/delete", method = RequestMethod.DELETE)
+    public String deleteIngredient(@PathVariable String recipeId, @PathVariable String id) {
+        logger.info("deleting ingredient id:" + id);
+        ingredientService.deleteById(Long.valueOf(recipeId), Long.valueOf(id));
+
+        return "redirect:/recipe/" + recipeId + "/ingredients";
+    }
+
+
 
 
 
